6a7d5911deb6404a854aae6425f7e1d932381b0c
mattanja <mattanja.kern@gmail.com>
2013-03-02 03:39:41 +0100

    update config, first msg sent

--- app/Global.scala
+++ app/Global.scala
@@ -7,43 +7,47 @@ import org.subethamail.smtp.server._
 object Global extends GlobalSettings {
 
   val smtpServer = new SMTPServer(new MessengerMessageHandlerFactory)
-    smtpServer.setPort(8025)
+  smtpServer.setPort(8025)
 
   override def onStart(app: Application) {
+    Logger.info("Starting application")
+
+    Logger.info("Configuration db.default.driver: " + Play.current.configuration.getString("db.default.driver"))
+    Logger.info("Configuration mail.host: " + Play.current.configuration.getString("mail.host").getOrElse("INVALID"))
     InitialData.insert()
-    
+
     Logger.info("Starting SMTP-server on port " + smtpServer.getPort() + "...")
     smtpServer.start()
   }
-  
+
   override def onStop(app: Application) {
     Logger.info("Stopping application...")
+
     Logger.info("Stopping SMTP-server...")
     smtpServer.stop()
   }
-  
-//  override def onError(request, ex) {
-//    return notFound
-//  }
+
+  //  override def onError(request, ex) {
+  //    return notFound
+  //  }
 }
 
 /**
- * Initial set of data to be imported 
+ * Initial set of data to be imported
  * in the sample application.
  */
 object InitialData {
-  
+
   def date(str: String) = new java.text.SimpleDateFormat("yyyy-MM-dd").parse(str)
-  
+
   def insert() = {
-    
-    if(User.findAll.isEmpty) {
+    if (User.findAll.isEmpty) {
+      Logger.info("Inserting initial user data...")
       Seq(
         User("mattanja.kern@gmail.com", "Matt", "secret"),
-        User("test@test.test", "test", "secret")
-      ).foreach(User.create)
+        User("test@test.test", "test", "secret")).foreach(User.create)
     }
-    
+
   }
-  
+
 }
--- app/MessengerMessageHandlerFactory.scala
+++ app/MessengerMessageHandlerFactory.scala
@@ -5,10 +5,25 @@ import org.subethamail.smtp.helper.SimpleMessageListenerAdapter
 import org.subethamail.smtp.helper.SimpleMessageListener
 import java.io.InputStream
 import play.api.Logger
+import play.api.Play
 import java.io.InputStreamReader
 import java.io.BufferedReader
+import javax.mail.MessagingException
+import javax.mail.PasswordAuthentication
+import javax.mail.Transport
+import javax.mail.Message
+import javax.mail.internet.MimeMessage
+import javax.mail.Session
+import javax.mail.internet.InternetAddress
+import com.sun.mail.smtp.SMTPMessage
+import javax.mail.Message.RecipientType
 
-class MessengerMessageHandlerFactory extends SimpleMessageListenerAdapter(new MessengerSimpleMessageListener) {  //extends MessageHandlerFactory {
+// For a more advanced message handling, check https://code.google.com/p/subetha/source/browse/trunk/src/org/subethamail/core/smtp/SMTPHandler.java
+
+/**
+ *
+ */
+class MessengerMessageHandlerFactory extends SimpleMessageListenerAdapter(new MessengerSimpleMessageListener) {
 }
 
 class MessengerSimpleMessageListener extends SimpleMessageListener {
@@ -16,23 +31,77 @@ class MessengerSimpleMessageListener extends SimpleMessageListener {
     Logger.info("Receiving message from: " + from + " for recipient: " + recipient)
     true
   }
-  
-  def deliver(from: String, recipient: String, data: InputStream): Unit = {
-	  Logger.info("Receiving message from: " + from + " for recipient: " + recipient + " with data:" + readInputStream(data))  
-  }
-  
-  def readInputStream(stream: InputStream): String = {
-	val is = new InputStreamReader(stream);
-	val sb=new StringBuilder();
-	val br = new BufferedReader(is);
-	var read = br.readLine();
 
-	while(read != null) {
-	    //System.out.println(read);
-	    sb.append(read);
-	    read = br.readLine();
-	}
-	
-	sb.toString();
+  def deliver(from: String, recipient: String, data: InputStream): Unit = {
+    val mailData = readInputStream(data)
+    Logger.info("Receiving message from: " + from + " for recipient: " + recipient + " with data:" + mailData)
+
+    sendMail("messenger@localhost", "mattanja.kern@gmail.com", data)
+  }
+
+  def sendMail(from: String, to: String, data: InputStream): Unit = {
+    Logger.info("sendMail")
+    val session = getSession
+    val transport = session.getTransport()
+
+    try {
+      transport.connect()
+
+      val msg = getMessage(from, to, data, session)
+      Logger.info("msg is prepared")
+
+      msg.saveChanges()
+
+      transport.sendMessage(msg, msg.getAllRecipients())
+      transport.close()
+    } catch {
+      case ex: MessagingException => throw new RuntimeException(ex)
+    } finally {
+      transport.close()
+    }
+  }
+
+  def getMessage(from: String, to: String, data: InputStream, session: Session): Message = {
+    Logger.info("getMessage")
+    val msg = new SMTPMessage(session, data)
+    msg.setFrom(new InternetAddress(from))
+    msg.setRecipients(Message.RecipientType.TO, to)
+    msg
+  }
+
+  /**
+   * Get Transport with a new session and configuration from app config.
+   */
+  def getSession(): Session = {
+    Logger.info("getSession")
+    val props = new java.util.Properties();
+    props.setProperty("mail.host", Play.current.configuration.getString("mail.host").get);
+    props.setProperty("mail.smtp.auth", Play.current.configuration.getBoolean("mail.smtp.auth").get.toString);
+    props.setProperty("mail.smtp.port", Play.current.configuration.getInt("mail.smtp.port").get.toString);
+    props.setProperty("mail.smtp.starttls.enable", Play.current.configuration.getBoolean("mail.smtp.starttls.enable").get.toString);
+    props.setProperty("mail.transport.protocol", Play.current.configuration.getString("mail.transport.protocol").get);
+    props.setProperty("mail.debug", Play.current.configuration.getBoolean("mail.debug").get.toString);
+    javax.mail.Session.getInstance(props, new javax.mail.Authenticator() {
+      override def getPasswordAuthentication(): PasswordAuthentication = {
+        new PasswordAuthentication(
+          Play.current.configuration.getString("mail.smtp.username").get,
+          Play.current.configuration.getString("mail.smtp.password").get)
+      }
+    })
+  }
+
+  def readInputStream(stream: InputStream): String = {
+    val is = new InputStreamReader(stream);
+    val sb = new StringBuilder();
+    val br = new BufferedReader(is);
+    var read = br.readLine();
+
+    while (read != null) {
+      //System.out.println(read);
+      sb.append(read);
+      read = br.readLine();
+    }
+
+    sb.toString();
   }
 }

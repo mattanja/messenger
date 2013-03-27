7048bd15e0d5774469d5f36cb1c6fca2c26a6643
kuhnen <andrekuhnen@gmail.com>
2013-03-27 17:59:49 -0300

    Added mime4j dependency

--- project/Build.scala
+++ project/Build.scala
@@ -12,6 +12,7 @@ object ApplicationBuild extends Build {
     jdbc,
     "com.typesafe" %% "play-plugins-mailer" % "2.1.0",
     "org.apache.commons" % "commons-email" % "1.3.1",
+     "org.apache.james" % "apache-mime4j" % "0.7.2",            
     anorm)
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      

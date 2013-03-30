db466ad5c6a10384a6e0631781e102008fdbf4c0
kuhnen <andrekuhnen@gmail.com>
2013-03-30 16:16:24 -0300

    Minor change. Fixed User create

--- app/models/User.scala
+++ app/models/User.scala
@@ -8,17 +8,15 @@ import anorm.SqlParser._
 import play.api.Logger
 
 case class User(
-    email: String,
-    name: String,
-    password: String
-)
+  email: String,
+  name: String,
+  password: String)
 
 /**
  * This uses ScalaAnorm, packaged with play! framework 2
  * https://github.com/playframework/Play20/wiki/ScalaAnorm
  */
 object User {
-
 
   /**
    * JsonFormat
@@ -36,31 +34,30 @@ object User {
   }
 
   // -- Parsers
-  
+
   /**
    * Parse a User from a ResultSet
    */
   val simple = {
     get[String]("user.email") ~
-    get[String]("user.name") ~
-    get[String]("user.password") map {
-      case email~name~password => User(email, name, password)
-    }
+      get[String]("user.name") ~
+      get[String]("user.password") map {
+        case email ~ name ~ password => User(email, name, password)
+      }
   }
-  
+
   // -- Queries
-  
+
   /**
    * Retrieve a User from email.
    */
   def findByEmail(email: String): Option[User] = {
     DB.withConnection { implicit connection =>
       SQL("select * from user where email = {email}").on(
-        'email -> email
-      ).as(User.simple.singleOpt)
+        'email -> email).as(User.simple.singleOpt)
     }
   }
-  
+
   /**
    * Retrieve all users.
    */
@@ -69,46 +66,26 @@ object User {
       SQL("select * from user").as(User.simple *)
     }
   }
-  
-  /**
-   * Authenticate a User.
-   */
+
   def authenticate(email: String, password: String): Option[User] = {
     DB.withConnection { implicit connection =>
-      SQL(
-        """
-         select * from user where 
-         email = {email} and password = {password}
-        """
-      ).on(
-        'email -> email,
-        'password -> password
-      ).as(User.simple.singleOpt)
+      SQL("select * from user where	email = {email} and password = {password}").on(
+          'email -> email,
+          'password -> password).as(User.simple.singleOpt)
     }
   }
-   
-  /**
-   * Create a User.
-   */
-  def create(user: User): Int = {
-    DB.withConnection { implicit connection => try {
-      SQL(
-        """
-          insert into user values (
-            {email}, {name}, {password}
-          )
-        """
-      ).on(
-        'email -> user.email,
-        'name -> user.name,
-        'password -> user.password
-      ).executeUpdate()
-    } catch {
-      case e:Exception => Logger.error("Could not create new user\n" + e); 0
-    }
-      
-      
-    }
+
+   def create(user: User): Int = {
+    DB.withConnection { implicit connection =>
+      try {
+        SQL("INSERT INTO user values ({email}, {name}, {password})").on(
+          'email -> user.email,
+          'name -> user.name,
+          'password -> user.password).executeUpdate()
+      } catch {
+        case e: Exception => Logger.error("Could not create new user\n" + e); 0
+      }
+   }
   }
-  
+
 }
